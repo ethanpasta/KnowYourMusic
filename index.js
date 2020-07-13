64be49148dad695fa73793bf09045ac8e2118275
ethanpasta <ethan.mayer9@gmail.com>
2020-07-13 12:56:29 +0300

    Change "app" folder name to "backend"

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const sio = io.of("/game");
-require("./app/middleware")(app, sio);
-require("./app/socket")(sio);
-require("./app/database");
+require("./backend/middleware")(app, sio);
+require("./backend/socket")(sio);
+require("./backend/database");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
